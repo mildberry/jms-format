7340ca8f77aee2bcb52f5550befc19e7bb00e9c7
Egor Zyuskin <ezyuskin@Mac-mini-Egor.local>
2016-09-14 15:34:56 +0600

    fix html parser

--- src/Format/HtmlFormat.php
+++ src/Format/HtmlFormat.php
@@ -2,14 +2,28 @@
 
 namespace Mildberry\JMSFormat\Format;
 
+use DOMDocument;
+use DOMElement;
+use DOMNamedNodeMap;
+use DOMNodeList;
+use DOMText;
+use Mildberry\JMSFormat\Exception\BadTagNameException;
+use Mildberry\JMSFormat\Item\AbstractItem;
+use Mildberry\JMSFormat\Item\BlockQuoteItem;
 use Mildberry\JMSFormat\Item\CollectionItem;
+use Mildberry\JMSFormat\Item\HeadLineItem;
+use Mildberry\JMSFormat\Item\ImageItem;
+use Mildberry\JMSFormat\Item\ParagraphItem;
+use Mildberry\JMSFormat\Item\TextItem;
 
 /**
  * @author Egor Zyuskin <e.zyuskin@mildberry.com>
  */
 class HtmlFormat implements FormatInterface
 {
-    CONST ALLOWED_TAGS = '<p><span><img><b><i><del><blockquote>';
+    const ROOT_NODE_ID = 'DOMRootBodyElement';
+
+    const ALLOWED_TAGS = '<p><span><img><b><i><del><blockquote><h1><h2><h3><h4>';
 
     /**
      * @param string $content
@@ -26,7 +40,7 @@ class HtmlFormat implements FormatInterface
      */
     public function toContent(CollectionItem $collection)
     {
-        return '';
+        return ''; //TODO: make this
     }
 
     /**
@@ -37,9 +51,140 @@ class HtmlFormat implements FormatInterface
     {
         $collection = new CollectionItem();
 
-//        $content = strip_tags($content, self::ALLOWED_TAGS);
-//        print $content;
+        foreach ($this->createDOMElementsByHtml($content) as $element) {
+            if ($element instanceof DOMText) {
+                if ($text = trim(strip_tags($element->nodeValue))) {
+                    $collection->push(new TextItem($text));
+                }
+            } else {
+                $collection->push($this->createItemFromDOMElement($element));
+            }
+        }
 
         return $collection;
+    }
+
+    /**
+     * @param string $content
+     * @return DOMElement
+     */
+    private function createDOMElementsByHtml($content)
+    {
+        $DOMDocument = new DOMDocument('1.0', 'UTF-8');
+
+        libxml_use_internal_errors(true);
+        $DOMDocument->loadHTML('<body id="'.self::ROOT_NODE_ID.'">'.strip_tags($content, self::ALLOWED_TAGS).'</body>');
+        libxml_clear_errors();
+
+        return $DOMDocument->getElementById(self::ROOT_NODE_ID)->childNodes;
+    }
+
+    /**
+     * @param DOMElement $element
+     * @return AbstractItem
+     */
+    private function createItemFromDOMElement($element)
+    {
+        $tagName = mb_strtolower($element->tagName);
+        $className = $this->getClassNameByTagName($tagName);
+
+        $item = $this->updateBlockModifiersByTagNam(new $className(strip_tags($element->nodeValue)), $tagName, $element->attributes);
+
+        if ($element->hasChildNodes()) {
+            foreach ($element->childNodes as $childNode) {
+                if (XML_ELEMENT_NODE === $childNode->nodeType) {
+                    $item->push($this->createItemFromDOMElement($childNode));
+                }
+            }
+        }
+
+        if ($item instanceof CollectionItem) {
+            $item = $this->updateCollectionBlocksByHTML($item, $this->getDOMElementHtmlValue($element));
+        }
+
+        return $item;
+    }
+
+    /**
+     * @param DOMElement $element
+     * @return string
+     */
+    private function getDOMElementHtmlValue(DOMElement $element)
+    {
+        $innerHTML = '';
+        foreach ($element->childNodes as $child) {
+            $innerHTML .= $element->ownerDocument->saveHTML($child);
+        }
+
+        return $innerHTML;
+    }
+
+    /**
+     * @param CollectionItem $item
+     * @param string $html
+     * @return CollectionItem
+     */
+    private function updateCollectionBlocksByHTML($item, $html)
+    {
+        $pos = strpos($html, '<');
+        if ($pos !== false) {
+            if ($pos > 0) {
+                $item->unshift(new TextItem(substr($html, 0, $pos)));
+            }
+            if ($html = substr($html, strrpos($html, '>')+1)) {
+                $item->push(new TextItem($html));
+            }
+        } else {
+            $item->push(new TextItem($html));
+        }
+
+        return $item;
+    }
+
+    /**
+     * @param AbstractItem $item
+     * @param string $tagName
+     * @param DOMNamedNodeMap $attributes
+     * @return AbstractItem
+     */
+    private function updateBlockModifiersByTagNam($item, $tagName, DOMNamedNodeMap $attributes)
+    {
+        switch ($tagName) {
+            case 'b': $item->setDecoration('bold'); break;
+            case 'i': $item->setDecoration('italic'); break;
+            case 'del': $item->setDecoration('del'); break;
+            case 'h1': $item->setWeight('lg'); break;
+            case 'h2': $item->setWeight('md'); break;
+            case 'h3': $item->setWeight('sm'); break;
+            case 'h4': $item->setWeight('xs'); break;
+            case 'img': $item->setSrc($attributes->getNamedItem('src')->nodeValue); break;
+        }
+
+        return $item;
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     * @throws BadTagNameException
+     */
+    private function getClassNameByTagName($name)
+    {
+        switch ($name) {
+            case 'span': case 'b': case 'i': case 'del':
+                return TextItem::class;
+            case 'h1': case 'h2': case 'h3': case 'h4':
+                return HeadLineItem::class;
+            case 'img':
+                return ImageItem::class;
+            case 'p':
+                return ParagraphItem::class;
+            case 'blockquote':
+                return BlockQuoteItem::class;
+            case 'body':
+                return CollectionItem::class;
+        }
+
+        throw new BadTagNameException();
     }
 }
--- tests/Unit/JMSFormatTest.php
+++ tests/Unit/JMSFormatTest.php
@@ -21,7 +21,7 @@ class JMSFormatTest extends PHPUnit_Framework_TestCase
     {
         $contentFormatter = $this->createFormatter();
         try {
-            $contentFormatter->convert('wrongFormat1', 'wrongFormat2', '<html></html>');
+            $contentFormatter->convert('wrongFormat1', 'wrongFormat2', $this->getHtmlText());
             $this->assertTrue(false);
         } catch (WrongFormatNameException $e) {
             $this->assertTrue(true);
@@ -31,7 +31,7 @@ class JMSFormatTest extends PHPUnit_Framework_TestCase
     public function testSuccessConvertFromHtmlToJson()
     {
         $contentFormatter = $this->createFormatter();
-        $this->assertEquals('{"version":"v1","content":[]}', $contentFormatter->convert('html', 'JMS', '<html></html>'));
+        $this->assertEquals('{"version":"v1","content":[{"block":"headline","modifiers":{"weight":"lg"},"content":[{"block":"text","modifiers":[],"content":"Header "},{"block":"text","modifiers":{"decoration":"italic"},"content":"1"}]},{"block":"paragraph","modifiers":[],"content":[{"block":"text","modifiers":[],"content":" Paragraph "},{"block":"text","modifiers":{"decoration":"bold"},"content":"1"},{"block":"text","modifiers":[],"content":"Span text!!!"},{"block":"image","modifiers":{"src":"https:\/\/www.ya.ru\/favicon.ico"}}]},{"block":"headline","modifiers":{"weight":"md"},"content":[{"block":"text","modifiers":[],"content":"Next header"}]},{"block":"blockquote","modifiers":[],"content":[{"block":"text","modifiers":[],"content":"Block quote text"}]}]}', $contentFormatter->convert('html', 'JMS', $this->getHtmlText()));
     }
 
     /**
@@ -40,5 +40,13 @@ class JMSFormatTest extends PHPUnit_Framework_TestCase
     private function createFormatter()
     {
         return new Formatter();
+    }
+
+    /**
+     * @return string
+     */
+    private function getHtmlText()
+    {
+        return '<h1>Header <i>1</i></h1><p> Paragraph <b>1</b> <span>Span text!!!</span> <img src="https://www.ya.ru/favicon.ico"></p><h2>Next header</h2><blockquote>Block quote text</blockquote>';
     }
 }
